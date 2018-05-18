8d9ca38c06ceaf535e16ba54bab00e42d558c49e
tuanhoangth1603 <38004836+tuanhoangth1603@users.noreply.github.com>
2018-05-18 23:31:08 +0700

    Initial commit

---
+++ GoogleDriveRestAPI-v3/Controllers/HomeController.cs
@@ -0,0 +1,118 @@
+﻿using GoogleDriveRestAPI_v3.Models;
+using SteganoWave;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using System.Linq;
+
+namespace GoogleDriveRestAPI_v3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserAccount account)
+        {
+
+            using (MVCEntities db = new MVCEntities())
+            {
+                var obj = db.UserAccounts.Where(a => a.Username.Equals(account.Username) && a.Password.Equals(account.Password)).FirstOrDefault();
+                if (obj != null)
+                {
+                    Session["UserID"] = obj.UserID.ToString();
+                    Session["Username"] = obj.Username.ToString();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username or Password is wrong.");
+                }
+            }
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserAccount account)
+        {
+            if (ModelState.IsValid)
+            {
+                using (MVCEntities db = new MVCEntities())
+                {
+                    var obj = db.UserAccounts.Where(a => a.Username.Equals(account.Username)).FirstOrDefault();
+                    var objEmail = db.UserAccounts.Where(a => a.Email.Equals(account.Email)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        ModelState.AddModelError("", "Username is taken. Try another.");
+                    }
+                    else if (objEmail != null)
+                    {
+                        ModelState.AddModelError("", "Email is taken. Try another.");
+                    }
+                    else
+                    {
+                        db.UserAccounts.Add(account);
+                        db.SaveChanges();
+                        ModelState.Clear();
+                        ModelState.AddModelError("", "Successfully registered.");
+                    }
+                }
+            }
+            return View();
+        }
+
+        
+        
+        [HttpGet]
+        public ActionResult Index()
+        {
+            
+            return View(GoogleDriveFilesRepository.GetDriveFiles());
+        }
+
+        [HttpPost]
+        public ActionResult DeleteFile(GoogleDriveFiles file)
+        {
+            GoogleDriveFilesRepository.DeleteFile(file);
+            return RedirectToAction("GetGoogleDriveFiles");
+        }
+         
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            GoogleDriveFilesRepository.FileUpload(file);
+            //Watermark wt = new Watermark();
+            //wt.btnHide_Click();
+
+            return RedirectToAction("GetGoogleDriveFiles");
+        }
+        
+
+        public void DownloadFile(string id)
+        {
+            if (Session["Username"].Equals("admin"))
+            {
+                string FilePath = GoogleDriveFilesRepository.DownloadGoogleFile(id);
+
+                Response.ContentType = "application/zip";
+                Response.AddHeader("Content-Disposition", "attachment; filename=" + Path.GetFileName(FilePath));
+                Response.WriteFile(System.Web.HttpContext.Current.Server.MapPath("~/GoogleDriveFiles/" + Path.GetFileName(FilePath)));
+                Response.End();
+                Response.Flush();
+            }
+            else
+            {
+                RedirectToAction("Login");
+            }
+            
+        }
+        
+    }
+}
---
+++ GoogleDriveRestAPI-v3/Watermark.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Text;
+
+using System.Threading;
+
+/// <summary>
+/// Summary description for Watermark
+/// </summary>
+/// 
+namespace SteganoWave{
+public class Watermark
+{
+	public Watermark()
+	{
+		
+    }
+    public void btnHide_Click(string pathFile )
+    {
+        //hide the message inside the carrier wave
+
+
+        
+
+            Stream sourceStream = null;
+            FileStream destinationStream = null;
+            WaveStream audioStream = null;
+            System.IO.Directory.GetCurrentDirectory();
+            //create a stream that contains the message, preceeded by its length
+            Stream messageStream = GetMessageStream();
+            //open the key file
+            string path = @"C:/Users/admin/Desktop/KTGT/GoogleDriveRestAPI-v3/GoogleDriveRestAPI-v3";
+            //Stream keyStream = new FileStream()
+            Stream keyStream = new FileStream( path + "/Key/keyfile.txt", FileMode.Open);
+            //Stream s = new FileStream("../music/Còn Đâu Vòng Tay (Remix).mp3");
+
+            try
+            {
+
+                //how man samples do we need?
+                long countSamplesRequired = WaveUtility.CheckKeyForMessage(keyStream, messageStream.Length);
+
+                if (countSamplesRequired > Int32.MaxValue)
+                {
+                    throw new Exception("Message too long, or bad key! This message/key combination requires " + countSamplesRequired + " samples, only " + Int32.MaxValue + " samples are allowed.");
+                }
+
+                //if (rdoSrcFile.Checked)
+                //{ //use a .wav file as the carrier
+                
+                //sourceStream = new FileStream(path + "/Music/Charge.wav", FileMode.Open);
+                sourceStream = new FileStream(pathFile, FileMode.Open);
+                
+                //}
+                //else
+                //{ //record a carrier wave
+                //    frmRecorder recorder = new frmRecorder(countSamplesRequired);
+                //    recorder.ShowDialog(this);
+                //    sourceStream = recorder.RecordedStream;
+                //}
+
+               
+                //create an empty file for the carrier wave
+                
+                //destinationStream = new FileStream(path + "/Encode/Charge.wav", FileMode.Create);
+                destinationStream = new FileStream(pathFile, FileMode.Create);
+
+                //copy the carrier file's header
+                audioStream = new WaveStream(sourceStream, destinationStream);
+                if (audioStream.Length <= 0)
+                {
+                    throw new Exception("Invalid WAV file");
+                }
+
+                //are there enough samples in the carrier wave?
+                if (countSamplesRequired > audioStream.CountSamples)
+                {
+                    String errorReport = "The carrier file is too small for this message and key!\r\n"
+                        + "Samples available: " + audioStream.CountSamples + "\r\n"
+                        + "Samples needed: " + countSamplesRequired;
+                    throw new Exception(errorReport);
+                }
+
+                //hide the message
+                WaveUtility utility = new WaveUtility(audioStream, destinationStream);
+                utility.Hide(messageStream, keyStream);
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            finally
+            {
+                if (keyStream != null) { keyStream.Close(); }
+                if (messageStream != null) { messageStream.Close(); }
+                if (audioStream != null) { audioStream.Close(); }
+                if (sourceStream != null) { sourceStream.Close(); }
+                if (destinationStream != null) { destinationStream.Close(); }
+                
+            }
+        }
+    private Stream GetMessageStream()
+    {
+        string msg = "hoangtuan"; 
+        BinaryWriter messageWriter = new BinaryWriter(new MemoryStream());
+        messageWriter.Write(msg.Length);
+        messageWriter.Write(Encoding.ASCII.GetBytes(msg));
+        messageWriter.Seek(0, SeekOrigin.Begin);
+        return messageWriter.BaseStream;
+    }
+
+    }
+}
+ 
+	
